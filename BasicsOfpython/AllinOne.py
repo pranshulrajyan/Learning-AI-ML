495463399b20d3edb50fa268df8d47fdd77f32a6
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-08-29 21:45:23 +0530

    Python basics practice

---
+++ BasicsOfpython/AllinOne.py
@@ -0,0 +1,15 @@
+print("Hii", "Welcome to Python Programming")
+name = "Pranshul" #name is a variable which stores the name of the user
+print("My name is", name)
+print(type(name)) #type() is a function which tells the type of variable
+age = 18
+print(name, age)
+#Print Sum
+a = 1
+b= 2
+sum = a + b
+print("The sum of", a, "and", b, "is", sum)
+ #input() is a function which takes input from the user
+#the type of input() is always string, so we need to convert it into int or float if we want to perform any mathematical operation on it
+country = input("enter your country name: ")
+print("I am from", country)
